8d3ca30ff36b52214085c0a21c22b8e1e03e041c
Kosta Korenkov <kosta@whoot.me>
2016-09-08 15:44:54 +0300

    fix: close sometimes is not clickable for proposal

--- src/js/coloredcoins/copayColoredCoins.js
+++ src/js/coloredcoins/copayColoredCoins.js
@@ -22,7 +22,7 @@ angular.module('copayAddon.coloredCoins')
           if (txp.customData && txp.customData.asset) {
             var value = txp.amountStr;
             var asset = txp.customData.asset;
-            txp.amountStr = asset.amount + " unit" + (asset.amount > 1 ? "s" : "") + " of " + asset.assetName + " (" + value + ")";
+            txp.amountStr = asset.amount + " unit" + (asset.amount > 1 ? "s" : "") + " of " + asset.assetName;
             txp.showSingle = true;
             txp.toAddress = txp.outputs[0].toAddress; // txproposal
             txp.address = txp.outputs[0].address;     // txhistory
