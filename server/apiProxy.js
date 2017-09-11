6d88a1d2d958573b117603b0d00d3545a726da03
Loning <aloning@gmail.com>
2017-09-11 16:57:47 +0800

    fix api proxy crash error

--- server/apiProxy.js
+++ server/apiProxy.js
@@ -6,6 +6,23 @@ var testnetProxy = httpProxy.createProxyServer({});
 
 var mainnetProxy = httpProxy.createProxyServer({});
 
+
+var config = function (proxy){
+  proxy.on('proxyReq', function(proxyReq, req) {
+    // keep a ref
+     req._proxyReq = proxyReq;
+  });
+
+  proxy.on('error', function(err, req, res) {
+  if (req.socket.destroyed && err.code === 'ECONNRESET') {
+    req._proxyReq.abort();
+  }
+  });
+}
+
+config(testnetProxy);
+config(mainnetProxy);
+
 var userAgent = function (proxyReq, req, res, options) {
   proxyReq.setHeader('User-Agent', req.headers['user-agent'] + ' ColoredCopay/' + version);
 };
